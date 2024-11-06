b4515ecadf87d0b867c7535e080a710ffc7c80d2
SaffronG <chase.harmer@students.snow.edu>
2024-11-06 10:07:16 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> list = new (File.ReadAllLines("src.txt"));
+
+Graph graph = new Graph();
+
+foreach (var item in list) {
+    var temp = item.Split(",");
+    graph.AddEge(temp[0], temp[1], int.Parse(temp[2]));
+}
+
+public class Graph {
+    public Dictionary<string, List<Edge>> AdjacencyList { get; set; }
+
+    public Graph() {
+        AdjacencyList = new();
+    }
+
+    public void AddEge(string Src, string Destination, int Weight = 1) {
+        if (!AdjacencyList.ContainsKey(Src))
+            AdjacencyList[Src] = new();
+        AdjacencyList[Src].Add(new Edge(Destination, Weight));
+    }
+}
+
+public class Edge {
+    public string Destination { get; set; }
+    public int Weight { get; set; }
+    public Edge(String Destination, int Weight) {
+        this.Destination = Destination;
+        this.Weight = Weight;
+    }
+}
